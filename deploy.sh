4343ee303d5108e2c0bffa03abbf11b27bee6eb6
Andrew <andrew@hunt.li>
2017-02-20 15:48:02 -0500

    Fix argument grouping

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+#IS_DEBUG=1
 
 . "$BASH_SCRIPTS_DIR/util.sh"
 
@@ -12,7 +13,7 @@ function terraform_destroy() {
   _send_terraform_command "destroy"
 }
 
-function ansible_provision() {
+function ansible_list_inventory() {
   cd_to_ansible
   _send_ansible_dynamic_inventory_command "--list"
 }
@@ -26,7 +27,11 @@ function ansible_refresh_cache() {
 }
 
 function ansible_ping() {
-  _send_ansible_command "tag_Name_mcserver" "ping"
+  _send_ansible_command "tag_Name_mcserver" "-m ping"
+}
+
+function ansible_run() {
+  _send_ansible_command "tag_Name_mcserver" "$@"
 }
 
 function _send_ansible_dynamic_inventory_command() {
@@ -36,7 +41,11 @@ function _send_ansible_dynamic_inventory_command() {
 
 function _send_ansible_command() {
   cd_to_ansible
-  AWS_PROFILE=minecraft EC2_INI_PATH=./config/ec2.ini ansible "$1" -i ./vendor/ec2.py -m "$2"
+  if [ "$IS_DEBUG" ]; then
+    AWS_PROFILE=minecraft EC2_INI_PATH=./config/ec2.ini ansible -u ec2-user -vvv "$1" -i ./vendor/ec2.py "${@:2}"
+   else
+    AWS_PROFILE=minecraft EC2_INI_PATH=./config/ec2.ini ansible -u ec2-user "$1" -i ./vendor/ec2.py "${@:2}"
+  fi
 }
 
 
@@ -52,10 +61,11 @@ function usage() {
     'apply'
     'destroy'
     Ansible Dynamic Inventory:
-    'provision'
+    'list-inventory'
     'host-info'
     'refresh-cache'
     Ansible
+    'run'
     'ping-all'"
 }
 
@@ -82,9 +92,9 @@ main() {
         terraform_destroy
       end_section
     ;;
-    'provision')
+    'list-inventory')
       begin_section "Provisioning EC2 instances"
-        ansible_provision
+        ansible_list_inventory
       end_section
     ;;
     'host-info')
@@ -95,6 +105,12 @@ main() {
     'ping-all')
       begin_section "Gathering host info"
          ansible_ping
+      end_section
+    ;;
+    'run')
+      begin_section "Running command"
+          echo "$@"
+         ansible_run "${@:2}"
       end_section
     ;;
     'refresh-cache')
