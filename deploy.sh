4b771a4d4f21daf2cdedefa59a4bd935173a756d
Andrew <andrew@hunt.li>
2017-02-20 13:48:41 -0500

    Reorganize deploy script and add get-info

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,70 @@
 #!/bin/bash
+ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-function apply() {
-  terraform apply -var-file=secrets.tfvars
+. "$BASH_SCRIPTS_DIR/util.sh"
+
+function terraform_apply() {
+  cd_to_terraform
+  terraform apply -var-file=./config/secrets.tfvars ./operations
 }
 
-function destroy() {
-  terraform destroy -var-file=secrets.tfvars
+function terraform_destroy() {
+  cd_to_terraform
+  terraform destroy -var-file=./config/secrets.tfvars ./operations
+}
+
+function ansible_provision() {
+  cd_to_ansible
+  AWS_PROFILE=minecraft EC2_INI_PATH=./config/ec2.ini ./vendor/ec2.py --list
+}
+
+function ansible_host_info() {
+  cd_to_ansible
+  AWS_PROFILE=minecraft EC2_INI_PATH=./config/ec2.ini ./vendor/ec2.py --host "$1"
+}
+
+function usage() {
+  echo "Write usage here"
+}
+
+function cd_to_terraform() {
+  goToPath "$ROOT_DIR/terraform"
+}
+
+function cd_to_ansible() {
+  goToPath "$ROOT_DIR/ansible"
 }
 
 main() {
-  operation="$1"
+  OPERATION="$1"
+  HOST="$2"
 
-  if [[ "$operation" == 'destroy' ]]; then
-    destroy
-  else
-    apply
-  fi
+  case "$OPERATION" in
+    'apply')
+      begin_section "Deploying infrastructure changes to EC2"
+        terraform_apply
+      end_section
+    ;;
+    'destroy')
+      begin_section "Destroying all project-related infrastructure on EC2"
+        terraform_destroy
+      end_section
+    ;;
+    'provision')
+      begin_section "Provisioning EC2 instances"
+        ansible_provision
+      end_section
+    ;;
+
+    'host-info')
+      begin_section "Gathering host info"
+         ansible_host_info "$HOST"
+      end_section
+    ;;
+    *)
+      usage
+    ;;
+  esac
 }
 
 main "$@"
