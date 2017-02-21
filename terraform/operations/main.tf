d804ea0e00e26b0e8644d5951f41321217d06d76
kzsh <kzsh@kzsh.dev>
2017-02-20 21:46:03 -0500

    Rename infrastructure to minecraft

--- terraform/operations/main.tf
+++ terraform/operations/main.tf
@@ -9,55 +9,31 @@ variable "app_name" {
   default = "minecraft"
 }
 
-resource "aws_vpc" "default" {
+resource "aws_vpc" "minecraft" {
   cidr_block = "10.0.0.0/16"
 }
 
-resource "aws_internet_gateway" "default" {
-  vpc_id = "${aws_vpc.default.id}"
+resource "aws_internet_gateway" "minecraft" {
+  vpc_id = "${aws_vpc.minecraft.id}"
 }
 
 resource "aws_route" "internet_access" {
-  route_table_id         = "${aws_vpc.default.main_route_table_id}"
+  route_table_id         = "${aws_vpc.minecraft.main_route_table_id}"
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = "${aws_internet_gateway.default.id}"
+  gateway_id             = "${aws_internet_gateway.minecraft.id}"
 }
 
-resource "aws_subnet" "default" {
-  vpc_id                  = "${aws_vpc.default.id}"
+resource "aws_subnet" "minecraft" {
+  vpc_id                  = "${aws_vpc.minecraft.id}"
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
 }
 
-# A security group for the ELB so it is accessible via the web
-resource "aws_security_group" "elb" {
-  name        = "terraform_example_elb"
-  description = "Used in the terraform"
-  vpc_id      = "${aws_vpc.default.id}"
-
-  # HTTP access from anywhere
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # outbound internet access
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
 # Our default security group to access
-# the instances over SSH and HTTP
-resource "aws_security_group" "default" {
-  name        = "terraform_example"
-  description = "Used in the terraform"
-  vpc_id      = "${aws_vpc.default.id}"
+# the instances over SSH and MC's default port
+resource "aws_security_group" "minecraft" {
+  name        = "minecraft_security_group"
+  vpc_id      = "${aws_vpc.minecraft.id}"
 
   # SSH access from anywhere
   ingress {
@@ -67,12 +43,12 @@ resource "aws_security_group" "default" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  # HTTP access from the VPC
+  # Minecraft access from anywhere
   ingress {
-    from_port   = 80
-    to_port     = 80
+    from_port   = 25565
+    to_port     = 25565
     protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/16"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   # outbound internet access
@@ -84,22 +60,7 @@ resource "aws_security_group" "default" {
   }
 }
 
-resource "aws_elb" "web" {
-  name = "minecraft-elb"
-
-  subnets         = ["${aws_subnet.default.id}"]
-  security_groups = ["${aws_security_group.elb.id}"]
-  instances       = ["${aws_instance.web.id}"]
-
-  listener {
-    instance_port     = 80
-    instance_protocol = "http"
-    lb_port           = 80
-    lb_protocol       = "http"
-  }
-}
-
-resource "aws_key_pair" "auth" {
+resource "aws_key_pair" "minecraft" {
   key_name   = "${var.key_name}"
   public_key = "${file(var.public_key_path)}"
 }
@@ -121,15 +82,15 @@ resource "aws_instance" "web" {
   ami = "${lookup(var.aws_amis, var.aws_region)}"
 
   # The name of our SSH keypair we created above.
-  key_name = "${aws_key_pair.auth.id}"
+  key_name = "${aws_key_pair.minecraft.id}"
 
   # Our Security group to allow HTTP and SSH access
-  vpc_security_group_ids = ["${aws_security_group.default.id}"]
+  vpc_security_group_ids = ["${aws_security_group.minecraft.id}"]
 
   # We're going to launch into the same subnet as our ELB. In a production
   # environment it's more common to have a separate private subnet for
   # backend instances.
-  subnet_id = "${aws_subnet.default.id}"
+  subnet_id = "${aws_subnet.minecraft.id}"
 
   # We run a remote provisioner on the instance after creating it.
   # In this case, we just install nginx and start it. By default,
